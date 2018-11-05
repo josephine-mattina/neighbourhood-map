371f419f7f678ffae1e130b68a955aac330e5888
Josephine Flood <jmgflood@gmail.com>
2018-11-05 19:34:09 +0000

    Change map dependencies to react-google-maps

--- src/Map.js
+++ src/Map.js
@@ -1,108 +1,73 @@
 import React, { Component } from 'react';
-import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+// import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { compose, withProps } from "recompose";
 import iconInactive from "./icons/marker.png";
 import iconActive from "./icons/active.png";
 
-// const LoadingContainer = props => (
-// 	<div className="map">
-// 		<p>Sorry, Google Maps can't load right now</p>
-// 	</div>
-// );
 
-const Markers = props => (
-	props.locations.map((location) => (
-		<Marker 
-			{...props}
-			onClick={props.onClick} //.bind(this, location)
-      		position={{lat: location.location.lat, lng: location.location.lng}}
-			key={location.id}
-      		icon={props.selectedItem ? (props.selectedItem.name === location.name ? iconActive : iconInactive) : iconInactive}
-      		name={location.name}
-      		address={location.location.address || "address unavailable"}
-		/>
-	)
+// const Markers = props => (
+// 	props.locations.map((location) => (
+// 		<Marker 
+// 			{...props}
+// 			onClick={props.onClick} //.bind(this, location)
+//       		position={{lat: location.location.lat, lng: location.location.lng}}
+// 			key={location.id}
+//       		icon={props.selectedItem ? (props.selectedItem.name === location.name ? iconActive : iconInactive) : iconInactive}
+//       		name={location.name}
+//       		address={location.location.address || "address unavailable"}
+// 		/>
+// 	)
 	
-));
+// ));
 
-export class GlaMap extends Component {
+const MapComponent = compose(
+	withProps({
+		googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDUsVzFs47MfEzmFIuQCTRfQx_3kXO69RM&v=3",
+		loadingElement: <div style={{ height: `100%` }} />,
+		containerElement: <div style={{ height: `100vh` }} className={'map'} />,
+		mapElement: <div style={{ height: `100%` }} />,
+	}),
+	withScriptjs,
+	withGoogleMap
+)((props) =>
+	<GoogleMap
+        role="application"
+        aria-label="Map of Glasgow City Centre"
+        tabIndex="-1"
+		defaultZoom={13}
+		defaultCenter={{
+			lat: 55.859292,
+			lng: -4.258055
+		}}
+		// containerStyle={{position: this.props.position}}
+	>
+		
+	</GoogleMap>
+)
 
-	// state = {
-	 
-	//     // selectedPlace: {},
-	//     // showingInfoWindow:false
-	// };
+class GlaMap extends Component {
 
-	markers = [];
+	// markers = [];
 
-	// onMarkerClick = (props, marker) => {
 
-	//     this.setState({
-	// 		selectedPlace: props,
-	// 		activeMarker: marker,
-	// 		showingInfoWindow: true
-	//     });
-
+	// onMarkerClick = (marker, location) => {
+	// 	console.log('This is marker', marker, 'This is location', location)
+	// 	this.props.handleMarkerClick(location)
 	// }
-
-	// TODO: Why is showingInfoWindow not working? 
-	// In APP infoWindow state is logged from lines 57 and 65 as true, but props passed in here do not reflect state?
-	
-	
-
-	onMarkerClick = (marker, location) => {
-		console.log('This is marker', marker, 'This is location', location)
-		this.props.handleMarkerClick(location)
-			//  this.setState({
-
-			// showingInfoWindow: true
-	  //   });
-
-	}
 
 	render() {
 
 		return(
-				<Map 
-			        role="application"
-			        aria-label="Map of Glasgow City Centre"
-			        tabIndex="-1"
-					google={this.props.google}
-					zoom={13}
-					initialCenter={{
-						lat: 55.859292,
-						lng: -4.258055
-					}}
-					className={'map'}
-					containerStyle={{position: this.props.position}}
-				>
-					<Markers
-						locations={this.props.locations}
-						refs={this.markers}
-						onClick={this.onMarkerClick.bind(this)}
-						selectedItem={this.props.selectedItem}
-					/>
-					<InfoWindow
-						marker={this.props.selectedItem}
-						visible= {this.props.showingInfoWindow}
-						className={'info'}
-						//open=true
-					>
-						<article>
-							<h2>{this.props.selectedItem.name}</h2>
-							<p>{this.props.selectedItem.address}</p>
-						</article>
-					</InfoWindow>
-				</Map>
+			<MapComponent
+
+			/>
 		)
 	}
 
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyDUsVzFs47MfEzmFIuQCTRfQx_3kXO69RM",
-  // LoadingContainer: LoadingContainer
-})(GlaMap)
-
+export default GlaMap;
 
 
 
